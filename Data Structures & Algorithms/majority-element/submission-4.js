c258b845e51c7b7f28d3c55e58068b3ececbfcb1
Prabuddha Kumar <63943167+Prabuddha16@users.noreply.github.com>
2026-09-04 22:37:28 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        // return this.brute(nums);
+        // return this.sorting(nums);
+        return this.hashing(nums);
+    }
+
+    hashing(nums) {
+        const map = new Map();
+        let res = 0;
+        let maxCount = 0;
+
+        for (let num of nums) {
+            // Set or Get Count
+            map.set(num, (map.get(num) || 0) + 1);
+
+            if (map.get(num) > maxCount) {
+                res = num;
+                maxCount = map.get(num);
+            }
+        }
+
+        return res;
+    }
+
+    sorting(nums) {
+        nums.sort((a, b) => a - b);
+        return nums[Math.floor(nums.length / 2)];
+    }
+
+    brute(nums) {
+        let n = nums.length;
+
+        for (let num of nums) {
+            // JS Array Inbuilt Method
+            let count = nums.reduce((acc, val) => acc + (val === num ? 1 : 0), 0);
+
+            // Check Majority
+            if (count > Math.floor(n / 2)) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}

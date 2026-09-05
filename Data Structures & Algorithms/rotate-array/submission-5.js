5953b5b5192e9932ac1e4328b793cf88398955fa
Prabuddha Kumar <63943167+Prabuddha16@users.noreply.github.com>
2026-09-06 00:20:41 +0530

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    rotate(nums, k) {
+        // return this.brute(nums, k);
+        return this.reversalAlgo(nums, k);
+    }
+
+    reversalAlgo(nums, k) {
+        let n = nums.length;
+        k %= n;
+
+        const reverse = (arr, start, end) => {
+            while (start < end) {
+                [arr[start], arr[end]] = [arr[end], arr[start]];
+                start++;
+                end--;
+            }
+        };
+
+        // 1. Reverse entire array
+        reverse(nums, 0, n - 1);
+
+        // 2. Reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // 3. Reverse remaining elements
+        reverse(nums, k, n - 1);
+    }
+
+    brute(nums, k) {
+        const n = nums.length;
+
+        // k is greater than n
+        k %= n;
+
+        // Rotate k times
+        for (let i = 0; i < k; i++) {
+            // Pick last element
+            let last = nums[n - 1];
+
+            // Iterate from last
+            for (let j = n - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+
+            // Assign last to first
+            nums[0] = last;
+        }
+    }
+}

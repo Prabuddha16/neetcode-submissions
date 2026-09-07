2c68975b12a230efc691d37ad0957725888493c6
Prabuddha Kumar <63943167+Prabuddha16@users.noreply.github.com>
2026-09-07 22:56:07 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        // return this.brute(numbers, target);
+        // return this.binarySearch(numbers, target);
+        return this.hashing(numbers, target);
+    }
+
+    hashing(arr, tar) {
+        const map = new Map();
+        for (let i = 0; i < arr.length; i++) {
+            const tmp = tar - arr[i];
+            if (map.has(tmp)) {
+                return [map.get(tmp), i + 1];
+            }
+            map.set(arr[i], i + 1);
+        }
+        return [];
+    }
+
+    binarySearch(arr, tar) {
+        for (let i = 0; i < arr.length; i++) {
+            let l = i + 1;
+            let r = arr.length - 1;
+            let temp = tar - arr[i];
+            while (l <= r) {
+                let mid = l + Math.floor((r - l) / 2);
+                if (arr[mid] === temp) {
+                    return [i + 1, mid + 1];
+                } else if (arr[mid] < temp) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return [];
+    }
+
+    brute(numbers, target) {
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] === target) {
+                    return [i + 1, j + 1];
+                }
+            }
+        }
+        return [];
+    }
+}

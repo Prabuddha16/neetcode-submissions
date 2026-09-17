0f7fe01dd6f40da8b7d75da30435ae22e29020cc
Prabuddha Kumar <63943167+Prabuddha16@users.noreply.github.com>
2026-09-18 00:23:23 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.js
@@ -0,0 +1,56 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        // return this.brute(nums);
+        // return this.hashing(nums);
+        return this.fastSlowPointer(nums);
+    }
+
+    fastSlowPointer(nums) {
+        // Phase 1: Detect cycle
+        let slow = nums[0];
+        let fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow !== fast);
+
+        // Phase 2: Find cycle entrance
+        slow = nums[0]; // reset slow pointer
+
+        while (slow !== fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+
+    hashing(nums) {
+        const set = new Set();
+
+        for (let num of nums) {
+            if (set.has(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+
+        return -1;
+    }
+
+    brute(nums) {
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (nums[i] === nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}

6456ee14e28b715864ac9809333aab2336893347
Prabuddha Kumar <63943167+Prabuddha16@users.noreply.github.com>
2026-09-21 00:41:57 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        // return this.brute(nums);
+        // return this.sorting(nums);
+        return this.hashing(nums);
+    }
+
+    brute(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    sorting(nums) {
+        nums.sort((a, b) => a - b);
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (nums[i] === nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    hashing(nums) {
+        const set = new Set();
+        for (const num of nums) {
+            if (set.has(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
